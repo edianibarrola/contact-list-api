fdc05052b4ba903c8fe1cec0ef780f208d280ed0
edianibarrola <edianibarrola@gmail.com>
2021-01-06 22:14:15 +0000

    update

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -57,6 +57,45 @@ const getState = ({ getStore, setStore }) => {
 					.catch(function(error) {
 						console.log("Looks like there was a problem: \n", error);
 					});
+			},
+			addNewContact: data => {
+				fetch("https://assets.breatheco.de/apis/fake/contact/", {
+					method: "POST", // or 'POST'
+					body: JSON.stringify({
+						full_name: data.name,
+						email: data.email,
+						agenda_slug: "Edian",
+						address: data.address,
+						phone: data.phone
+					}), // data can be `string` or {object}!
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
+					.then(function(response) {
+						if (!response.ok) {
+							throw Error(response.statusText);
+						}
+						return response.json();
+					})
+					.then(jsonifiedResponse => {
+						console.log(jsonifiedResponse);
+
+						fetch("https://assets.breatheco.de/apis/fake/contact/agenda/Edian")
+							.then(function(response) {
+								if (!response.ok) {
+									throw Error(response.statusText);
+								}
+								return response.json();
+							})
+							.then(jsonifiedResponse => setStore({ agenda: jsonifiedResponse }))
+							.catch(function(error) {
+								console.log("Looks like there was a problem: \n", error);
+							});
+					})
+					.catch(function(error) {
+						console.log("Looks like there was a problem: \n", error);
+					});
 			}
 		}
 	};
--- src/js/views/AddContact.js
+++ src/js/views/AddContact.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { GlobalState } from "../store/appContext";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
 export const AddContact = () => {
+	const { store, actions } = useContext(GlobalState);
+
+	const info = {
+		name: "",
+		email: "",
+		phone: "",
+		address: ""
+	};
+
+	const [contact, setContact] = useState({
+		name: info.name,
+		email: info.email,
+		phone: info.phone,
+		address: info.address
+	});
+
+	const handleChange = e => {
+		setContact({ ...contact, [e.target.name]: e.target.value });
+	};
+
 	return (
 		<div className="container">
 			<div>
@@ -9,21 +31,52 @@ export const AddContact = () => {
 				<form>
 					<div className="form-group">
 						<label>Full Name</label>
-						<input type="text" className="form-control" placeholder="Full Name" />
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Full Name"
+							name="name"
+							value={contact.name}
+							onChange={handleChange}
+						/>
 					</div>
 					<div className="form-group">
 						<label>Email</label>
-						<input type="email" className="form-control" placeholder="Enter email" />
+						<input
+							type="email"
+							className="form-control"
+							placeholder="Enter email"
+							name="email"
+							value={contact.email}
+							onChange={handleChange}
+						/>
 					</div>
 					<div className="form-group">
 						<label>Phone</label>
-						<input type="phone" className="form-control" placeholder="Enter phone" />
+						<input
+							type="phone"
+							className="form-control"
+							placeholder="Enter phone"
+							name="phone"
+							value={contact.phone}
+							onChange={handleChange}
+						/>
 					</div>
 					<div className="form-group">
 						<label>Address</label>
-						<input type="text" className="form-control" placeholder="Enter address" />
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Enter address"
+							name="address"
+							value={contact.address}
+							onChange={handleChange}
+						/>
 					</div>
-					<button type="button" className="btn btn-primary form-control">
+					<button
+						type="button"
+						className="btn btn-primary form-control"
+						onClick={() => actions.addNewContact(contact)}>
 						save
 					</button>
 					<Link className="mt-3 w-100 text-center" to="/">
