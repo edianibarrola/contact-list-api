97da713550218695adeee6c854eeb2fdc75dfd5c
edianibarrola <edianibarrola@gmail.com>
2021-01-06 15:32:16 +0000

    update

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,11 +1,23 @@
 const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
+			agenda: null
 			//Your data structures, A.K.A Entities
 		},
 		actions: {
-			//(Arrow) Functions that update the Store
-			// Remember to use the scope: scope.state.store & scope.setState()
+			loadInitialData: () => {
+				fetch("https://assets.breatheco.de/apis/fake/contact/agenda/Edian")
+					.then(function(response) {
+						if (!response.ok) {
+							throw Error(response.statusText);
+						}
+						return response.json();
+					})
+					.then(jsonifiedResponse => setStore({ agenda: jsonifiedResponse }))
+					.catch(function(error) {
+						console.log("Looks like there was a problem: \n", error);
+					});
+			}
 		}
 	};
 };
